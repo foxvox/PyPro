f3c7a24fe9cb2b085f823c9a29b80a3999894b09
alex <windev@nate.com>
2024-09-24 06:32:32 +0900

    rand max_of

--- max_of_test_randint.py
+++ max_of_test_randint.py
@@ -7,8 +7,8 @@ lo = int(input('난수의 최솟값을 입력하세요.: '))
 hi = int(input('난수의 최댓값을 입력하세요.: '))
 x = [None] * num
 
-for i in range(num):
+for i in range(num): 
     x[i] = random.randint(lo, hi)
 
-print(f'{x}')
+print(f'{(x)}')
 print(f'이 가운데 최댓값은 {max_of(x)}입니다.')
